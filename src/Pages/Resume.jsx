dc35ba0f7c78f07d656c654a1dde3c9e03a7b9ec
David Kerr <kerrky@gmail.com>
2024-06-21 17:32:54 -0400

    Fixed Resume

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -2,8 +2,8 @@ function Resume() {
   const pdfURL = "./Images/resume.pdf";
 
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
-      <iframe src={pdfURL} className="w-full lg:w-4/5 h-full" />
+    <div className="flex items-center justify-center h-screen w-screen p-2">
+      <iframe src={pdfURL} className="w-full lg:w-4/5 h-full border-none" />
     </div>
   );
 }
