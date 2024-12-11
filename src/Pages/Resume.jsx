d77751c4835baf04312775c25316173911c64a48
David Kerr <kerrky@gmail.com>
2024-12-11 11:53:36 -0500

    Updated resume

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,9 +1,9 @@
 function Resume() {
-  const pdfURL = "./Images/DavidKerrResumeU.pdf";
+  const pdfURL = "./Images/ResumeA.pdf";
 
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="flex items-center justify-center h-screen w-screen">
+    <div className="overflow-x-hidden antialiased text-neutral-300 selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="flex items-center justify-center w-screen h-screen">
         <iframe src={pdfURL} className="w-full h-full border-none" />
       </div>
     </div>
